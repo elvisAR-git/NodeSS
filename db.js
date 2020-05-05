8b5827374058f1cd809ec112c16fc325699e271f
elvisAR-git <alanray459@gmail.com>
2020-05-05 12:17:46 +0300

    innitial commit

---
+++ db.js
@@ -0,0 +1,143 @@
+const mysql = require('mysql')
+
+
+function connectToDatabase(databaseName)
+{
+    return new Promise((resolve, reject) =>
+    {
+        var database = mysql.createConnection(
+            {
+                host: "localhost",
+                user: "elvis",
+                password: "moraaelvis",
+                database: databaseName,
+                charset: 'utf8mb4',              //correct charset for emojis
+                collation: 'utf8mb4_unicode_ci',
+            }, (err) =>
+        {
+            if (err) reject(err)
+        }
+        )
+        resolve(database)
+
+    })
+}
+
+
+
+function getCollection(database, table, limit = 0)
+{
+    if (limit == 0) limit = "*"
+    limit = limit.toString()
+
+    var query = `SELECT ${limit} FROM ${table}`
+
+    return new Promise((resolve, reject) =>
+    {
+        // running query
+        database.query(query, (err, result, fields) =>
+        {
+            if (err) reject(err)
+            resolve(result)
+        })
+    })
+}
+
+function Insert(database, table, data = {})
+{
+    return new Promise((resolve, reject) =>
+    {
+        var sql = "INSERT INTO " + table + " SET ?";
+        database.query(sql, data, (err, result) =>
+        {
+            if (err) reject(err)
+            resolve(result)
+        })
+    })
+
+}
+
+
+function FetchViaId(database, table, id, alias = undefined)
+{
+    var sql = ""
+    if (!alias)
+    {
+        sql = "SELECT * FROM " + table + " WHERE id=" + id
+    } else
+    {
+        sql = "SELECT * FROM " + table + " WHERE " + alias + "=" + id
+    }
+    return new Promise((resolve, reject) =>
+    {
+        database.query(sql, (err, result) =>
+        {
+            if (err) reject(err)
+            resolve(result[0])
+        })
+    })
+}
+
+
+function Delete(database, table, column, condition)
+{
+    var sql = `DELETE FROM ${table} WHERE ${column}${condition}`
+    return new Promise((resolve, reject) =>
+    {
+        database.query(sql, (err, result) =>
+        {
+            if (err) reject(err)
+            resolve(result)
+        })
+    })
+}
+
+function Filter(database, table, constraint)
+{
+    // constraint is an evaluated string e.g id=5
+    var sql = `SELECT * FROM ${table} WHERE ${constraint}`
+    return new Promise((resolve, reject) =>
+    {
+        database.query(sql, (err, result) =>
+        {
+            if (err) reject(err)
+            resolve(result)
+
+        })
+    })
+}
+
+
+module.exports = {
+    connect: async (databaseName) =>
+    {
+        let database = await connectToDatabase(databaseName)
+        return database
+    },
+    fetch: async (database, table, limit) =>
+    {
+        let result = await getCollection(database, table, limit)
+        return result
+    },
+    push: async (database, table, data = {}) =>
+    {
+        let result = await Insert(database, table, data)
+        return result
+    },
+    get: async (database, table, id, alias) =>
+    {
+        let result = await FetchViaId(database, table, id, alias)
+        return result
+    },
+    filter: async (database, table, constraint) =>
+    {
+        let result = await Filter(database, table, constraint)
+        return result
+    },
+    delete: async (database, table, column, condition) =>
+    {
+        let result = await Delete(database, table, column, condition)
+        return result
+    }
+}
+
---
+++ index.js
@@ -0,0 +1,271 @@
+const express = require('express')
+const validate = require('./validators.js').validate
+var Db = require('./db.js')
+
+let MyDB
+
+Db.connect("ARDB").then((database) =>
+{
+    MyDB = database
+}).catch((err) =>
+{
+    console.log("Could not connect to Database: ", err)
+})
+
+var app = express()
+app.use(express.json({ type: 'application/json' }))
+app.use(express.urlencoded())
+
+var server = require('http').createServer(app)
+var io = require('socket.io').listen(server)
+
+var connections = []
+
+
+// Http common
+
+app.post("/create", (req, res) =>
+{
+    var user = req.body
+    var check = validate(user)
+
+    if (!check.error)
+    {
+
+        Db.push(MyDB, "Users", user).then((result) =>
+        {
+            res.status(201).send("Successfully created user profile for " + user.username)
+        }).catch((err) =>
+        {
+            res.status(500).send(err)
+        })
+    } else
+    {
+        res.status(401).send(check.error.message)
+    }
+
+
+})
+
+
+
+
+server.listen(3000, "192.168.100.35");
+console.log("Server Running")
+
+var alive = []
+
+var connected = []
+// middleware
+
+
+io.sockets.on('connect', (socket) =>
+{
+    connections.push(socket)
+    console.log("Connected: %s sockets connected", connections.length)
+
+    socket.on('disconnect', (data) =>
+    {
+        // disconnected
+        console.log("Disconnected from -->", socket.id)
+        connections.splice(connections.indexOf(socket), 1)
+        console.log('Disconnected: %s sockets connected', connections.length)
+
+        var ls = []
+        connected.forEach(session =>
+        {
+            if (session.sock == socket.id)
+            {
+                io.emit("disconnected", session)
+
+                console.log("DISCONECT<<>>", { UID: session.UID, sock: session.sock })
+
+            } else
+            {
+                ls.push(session)
+            }
+        });
+        connected = ls
+    })
+    socket.on('login', (credentials) =>
+    {
+        credentials = JSON.parse(credentials)
+        var check = validate(credentials)
+
+        if (check.error)
+        {
+            socket.emit("Error", check.error.message)
+        } else
+        {
+            // io.sockets.emit('new message', { msg: data })
+            Db.fetch(MyDB, "Users", 0).then((data) =>
+            {
+                data.forEach(account =>
+                {
+                    if (account.username === credentials.username && credentials.password == account.password)
+                    {
+                        // accept
+                        account.password = undefined
+                        account.sock_id = socket.id
+                        socket.emit("accept login", account)
+                        alive.push({ user: account, sock: socket })
+                        connected.push({ UID: account.UID, sock: socket.id })
+                        io.emit("connected", { UID: account.UID, sock: socket.id })
+
+                    }
+                });
+            })
+        }
+
+    })
+
+    socket.on('message', (message) =>
+    {
+        message = JSON.parse(message)
+        // pushing to database
+        Db.push(MyDB, "Messages", message).then((result) =>
+        {
+            // fetching the pushed item
+            Db.get(MyDB, "Messages", result.insertId).then((data) =>
+            {
+                alive.forEach(acc =>
+                {
+                    if (acc.user.UID == data.receiver || acc.user.UID == data.sender)
+                    {
+                        console.log("Broadcasting to --->", acc.user.username)
+                        acc.sock.emit("new message", data)
+                    }
+                });
+            }).catch((err) =>
+            {
+                socket.emit("Error", err.sqlMessage)
+            })
+        }).catch((err) =>
+        {
+            socket.emit("Error", err.sqlMessage)
+        })
+
+    })
+
+    //  this event emits all the alive sockets regardless of the relationship between the sockets
+    // NOT recomended
+    socket.on("get alive", (account) =>
+    {
+        var acc = []
+        alive.forEach(account =>
+        {
+            acc.push(account.user)
+        });
+        socket.emit("alive", acc)
+    })
+
+    socket.on("get related", (account) =>
+    {
+        account = JSON.parse(account)
+        let Matches = []
+
+        Db.fetch(MyDB, "Users", 0).then((Users) =>
+        {
+            Db.fetch(MyDB, "Messages", 0).then((Messages) =>
+            {
+                Messages.forEach(message =>
+                {
+                    Users.forEach(user =>
+                    {
+                        if (message.sender == account.UID && message.receiver == user.UID)
+                        {
+                            // Drop the password field
+                            user.password = undefined
+                            if (!Matches.includes(user))
+                            {
+                                Matches.push(user)
+                            }
+                        } else if (message.receiver == account.UID && message.sender == user.UID)
+                        {
+                            // Drop the password field
+                            user.password = undefined
+                            if (!Matches.includes(user))
+                            {
+                                Matches.push(user)
+                            }
+                        }
+                    });
+                });
+                socket.emit("related", Matches)
+            })
+
+        }).catch((err) =>
+        {
+            console.log(err)
+        })
+
+    })
+
+    socket.on("get inbox", (account) =>
+    {
+        var threads = []
+
+        var constraint = `receiver=${account.UID} OR sender=${account.UID}`
+
+        Db.filter(MyDB, "Messages", constraint).then((result) =>
+        {
+            socket.emit("inbox", result)
+
+        }).catch((err) =>
+        {
+            console.log(err)
+        })
+    })
+
+
+    socket.on("delete", (id) =>
+    {
+        Db.delete(MyDB, "Messages", "id", `=${id}`).then((result) =>
+        {
+            console.log(result)
+        }).catch((err) =>
+        {
+            console.log(err)
+        })
+
+    })
+
+
+    socket.on("get messages for", (id1, id2) =>
+    {
+        var constraint = `(receiver=${id1} AND sender=${id2}) OR (receiver=${id2} AND sender=${id1})`
+        Db.filter(MyDB, "Messages", constraint).then((result) =>
+        {
+            socket.emit("messages for", result)
+
+        }).catch((err) =>
+        {
+            socket.emit("Error", err)
+        })
+    })
+
+
+    socket.on("get profile", (email) =>
+    {
+        console.log("Profile fetch---->", email)
+        Db.get(MyDB, "Users", `"${email}"`, "email").then((result) =>
+        {
+            connected.forEach(session =>
+            {
+                if (session.UID == result.UID)
+                {
+                    // online
+                    result.online = "true"
+                    result.sock_id = session.sock
+                }
+            });
+            console.log(connected)
+            socket.emit('profile', result)
+        }).catch((err) =>
+        {
+            console.log(err)
+        })
+    })
+
+
+})
